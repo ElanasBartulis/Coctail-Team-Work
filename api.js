052515bb9cd9c46f75f273a7e4f2f04e858b2ad1
ElanasB <elanasbar@gmail.com>
2024-10-25 15:43:02 +0300

    NewObject gavimas

--- api.js
+++ api.js
@@ -1,11 +1,25 @@
 async function getApi() {
   const promise = await fetch("https://restcountries.com/v3.1/all");
   const response = await promise.json();
-  response.map((value) => {
-    console.log({
+
+  const newObject = response.map((value) => {
+    return {
       name: value.name.common,
       population: value.population,
-      currency: value.currencies,
-    });
+      currency: value.currencies
+        ? value.currencies[Object.keys(value.currencies)[0]]?.name
+        : "Unknown",
+      currencySymbol: value.currencies
+        ? value.currencies[Object.keys(value.currencies)[0]]?.symbol
+        : "Unknown",
+      capital: value.capital?.[0] || "No Capital",
+      time: "12:00",
+      language: value.languages
+        ? Object.values(value.languages).toString()
+        : "Unknown",
+      flagImg: value.flags.svg,
+    };
   });
+
+  console.log(newObject);
 }
